3e7186fde09b0f74105483e8bdb8b125cb9305bb
Kevin Downey <redchin@gmail.com>
2009-11-30 19:09:16 -0800

    WIP

--- a.lisp
+++ a.lisp
@@ -1,46 +1,88 @@
+;php ./compiler.php < a.lisp | beautify_php -d -i 2 > a.php
+
+(function get (thing key) (inline "$thing[$key]"))
+
+(function call_closure (closure x)
+  (set! args (array))
+  (inline "foreach($x as $key=>$value) $args[$closure[1][$key]] = $value")
+  (set! t (inline "$closure[0] == null"))
+  (set! w (if (env t) (array) (get closure 0)))
+  (call_user_func (get closure 2) (array_merge w args)))
+
+(function call ()
+  (set! x (func_get_args))
+  (set! closure (get x 0))
+  (array_shift x)
+  (if (function_exists closure)
+    (call_user_func_array closure x)
+    (call_closure closure x)))
+
+(function err (x)
+  (file_put_contents "php://stderr" x)
+  nil)
+
+(function pr (stream string)
+  (file_put_contents stream string)
+  nil)
+
+;;;;;;;;;;;;;;;
 (print "<pre>")
+;;;;;;;;;;;;;;;
 
-(function println [x]
+(function println (x)
   (print x)
-  (print "\n"))
+  (print "\n")
+  nil)
 
-(function closure [environment args name]
-  (array environment args name))
+(define first (λ (list) (get list 0)))
 
-(function get [thing key] (inline "$thing[$key]"))
+(define rest (λ (list) (array_shift list) list))
 
-(function first [list] (get list 0))
+(define = (λ (a b) (inline "$a == $b")))
 
-(function rest [list-]
-  (array_shift list-)
-  list-)
+(define empty?
+  (λ (list) 
+    (if (= 0 (sizeof list))
+      true
+      false)))
 
-(function = [a b]
-  (inline "$a == $b"))
-
-(function empty? [list]
-  (if (= 0 (sizeof list))
-    true
-    false))
-
-(def reduce
-  (fn [fun init list-]
-    (if (empty? list-)
+(define reduce
+  (λ (fun init list)
+    (if (empty? list)
       init
-      (recur fun (fun init (first list-)) (rest list-)))))
+      (recur fun (fun init (first list-)) (rest list)))))
 
-(def + (fn [a b] (+ a b)))
+(define cons
+  (λ (a b)
+    ((λ (list) (array_unshift list a) list)
+     (if (= null b) (array) b))))
 
-;(function + [a b] (+ a b))
+(define map
+  (λ (f l)
+    (if (empty? l)
+      l
+      (cons (f (first l))
+            (map f (rest l))))))
 
-(def x
-  (fn [w]
-    (if (= 0 w)
-      w
-      (recur (- w 1)))))
+(define filter
+  (λ (pred list)
+    (if (pred (first list))
+      (cons (first list) (filter pred (rest list)))
+      (filter pred (rest list)))))
+
+(define + (λ (a b) (+ a b)))
+
+(define ++ (λ (x) (+ 1 x)))
+
+(define -- (λ (x) (- x 1)))
+
+(define a (λ (& x) x))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (println (first '(1 2 3)))
 (println (reduce + 0 '(1 2 3)))
-(println (x 10))
+(println (reduce + 0 (map ++ '(1 2 3))))
+(println (((λ (x) (λ () x)) 10)))
 (println "foo")
-
+(pr "php://stdout" "bar\n")
